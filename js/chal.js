855a059b0e744dbdf9ae331d19633f2a3d05453e
RonenZubkov <ronenownz@gmail.com>
2016-06-13 21:01:05 +0300

    HomePage, localStorage bugged.

--- js/chal.js
+++ js/chal.js
@@ -1,56 +1,64 @@
+//
+// var gChals = [
+//     {
+//         id: 'game1.html',
+//         name: 'The Garden',
+//         isSolved: true
+//     },
+//     {
+//         id: 'game2.html',
+//         name: 'The Forest',
+//         isSolved: false
+//     },
+//     {
+//         id: 'game3.html',
+//         name: 'The Beach',
+//         isSolved: false
+//     },
+//     {
+//         id: 'game4.html',
+//         name: 'Some thing else',
+//         isSolved: false
+//     }
+// ];
+//
+// localStorage.chals = JSON.stringify(gChals);
 
-var gChals = [
-    {
-        id: 'game1.html',
-        name: 'The Garden',
-        isSolved: true
-    },
-    {
-        id: 'game2.html',
-        name: 'The Forest',
-        isSolved: false
-    },
-    {
-        id: 'game3.html',
-        name: 'The Beach',
-        isSolved: false
-    },
-    {
-        id: 'game4.html',
-        name: 'Some thing else',
-        isSolved: false
-    }
-];
+var gChals = null;
 
 function init(){
 
-    if(getFromStorage('Challenge')){
+    var chals = getFromStorage('challenge');
+
+    if(chals){
         console.log('Exists');
-        gChals = getFromStorage('Challenge');
+        gChals = chals;
         console.log(gChals);
     }
 
 }
 
-function getChals(value) {
-
-    for(var i = 0; i < value; i++){
-        if(gChals[i].isSolved === false)return;
-    }
-    // saveToStorage('Challenge',gChals);
-    window.location.href = gChals[value].id;
+function getChals() {
+    if (gChals === null) gChals = getFromStorage('chals');
+    return gChals;
 }
 
 
-// function getChalById(chalId) {}
+// function getChalById() {
 //
+//     gChals.filter(function(chals){
+//         return chals.id ===
+//     })
+//
+// }
+
 // function renderChals() {}
 
 function reportSolved(chalId){
 
     gChals[chalId].isSolved = true;
 
-    saveToStorage('Challenge',gChals);
+    saveToStorage('challenge',gChals);
     window.location.href = 'index.html';
     console.log(gChals);
 
