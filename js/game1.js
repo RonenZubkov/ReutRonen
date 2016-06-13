fd897e7bbb33b0b3725163a766d94faf9ed555db
RonenZubkov <ronenownz@gmail.com>
2016-06-13 23:26:20 +0300

    Game 1 - Skeleton.

--- js/game1.js
+++ js/game1.js
@@ -3,140 +3,78 @@
  */
 'use strict';
 
+var gCurrQuest = 0;
 var gQuestsTree = [
     {
         id: 1,
         txt: 'Who was created by Gepetto the woodcarver??',
-        correctAnswer: 'Pinokio'
+        opts:['a','b','c','Pinokio'],
+        correctOpt: 'Pinokio',
+        img:''
     },
 
     {
         id: 2,
         txt: 'Who painted the Mona Lisa?',
-        correctAnswer: 'Leonardo da Vinci'
+        opts:['a','Leonardo da Vinci','c','d'],
+        correctOpt: 'Leonardo da Vinci',
+        img:''
     },
 
     {
         id: 3,
         txt: 'Who is the bear in The Jungle Book??',
-        correctAnswer: 'Balue'
+        opts:['a','b','Balue','d'],
+        correctOpt: 'Balue',
+        img:''
     }
 ];
 
 
 
 
-
-// var gPrevQuest;
-// var gCurrQuest;
-// var gLastRes;
-
-
-$(document).ready(function(){
-    var $elFirstQues = $('.question1');
-    //Will be random question.
-    $elFirstQues.html(gQuestsTree[0].txt);
-
-    // var text = element.textContent;
-    // element.textContent = "i love jQuery (4u :P )";
+function init(){
+    renderQuestion();
+}
 
 
-    var $elCorrectBtn = $('.randomBtn1');
-    //Will be random answer.
-    $elCorrectBtn.html(gQuestsTree[0].correctAnswer);
+function renderQuestion(){
+    var elOptions = document.querySelectorAll('.options');
 
-    // gQuestsTree.yes = createQuest('Putin!');
-    // gQuestsTree.no = createQuest('Um Kultum!!');
-    //
-    // gPrevQuest = null;
-    // gCurrQuest = gQuestsTree;
-});
-
-function checkAnswer(value){
-    console.log(value);
-    var correctAnswer = ($('.randomBtn1').html());
-    console.log(correctAnswer);
-    if(correctAnswer === gQuestsTree[0].correctAnswer)alert('Correct!');
+    [].forEach.call(elOptions, function(elOption,i) {
+        elOption.innerHTML = gQuestsTree[gCurrQuest].opts[i];
+    });
 
 }
 
 
-//
-// function createQuest(txt) {
-//     return {
-//         txt: txt,
-//         yes: null,
-//         no: null
-//     }
-// }
-//
-//
-// function startGuessing() {
-//     //alert('Starting')
-//     var $elGameStart = $('.gameStart');
-//     $elGameStart.hide();
-//
-//     var $elGameQuest = $('.gameQuest');
-//
-//     renderQuest();
-//
-//     $elGameQuest.show();
-// }
-//
-// function renderQuest() {
-//     var $elGameQuest = $('.gameQuest');
-//     var $elQuestTxt = $elGameQuest.children('h2');
-//     $elQuestTxt.html(gCurrQuest.txt);
-// }
-//
-// function userResponse(res) {
-//
-//     // If this node has no children
-//     if (gCurrQuest.yes === null) {
-//         if (res === 'yes') {
-//             alert('Yes, I knew it!');
-//         } else {
-//             //alert('I dont know...')
-//             var $elGameNewQuest = $('.gameNewQuest');
-//             var $elGameQuest = $('.gameQuest');
-//             $elGameQuest.hide();
-//             $elGameNewQuest.show();
-//
-//         }
-//     } else {
-//         gPrevQuest = gCurrQuest;
-//         gCurrQuest = gCurrQuest[res];
-//         gLastRes   = res;
-//         renderQuest();
-//     }
-// }
-//
-// function addPerson() {
-//     var newPersonName   = $('#newPerson').val();
-//     var newQuestTxt     = $('#newQuest').val();
-//
-//     var newQuest        = createQuest(newQuestTxt);
-//     var newGuess        = createQuest(newPersonName);
-//
-//     newQuest.yes    = newGuess;
-//     newQuest.no     = gCurrQuest;
-//
-//     gPrevQuest[gLastRes]  = newQuest;
-//
-//     restartGame();
-//
-//
-//     //console.log('newPerson', newPersonName);
-//     //console.log('newQuest', newQuestTxt);
-//
-// }
-//
-//
-//
-// function restartGame() {
-//     $('.gameNewQuest').hide();
-//     $('.gameStart').show();
-//     gCurrQuest = gQuestsTree;
-//     gPrevQuest = null;
-//     gLastRes = null;
-// }
+function checkAnswer(el){
+
+    var elInpAnswer = el;
+
+    if(elInpAnswer === gQuestsTree[gCurrQuest].correctOpt){
+        alert('Correct!');
+        gCurrQuest++;
+        renderQuestion();
+    }
+
+    else{
+        gameOver();
+    }
+
+}
+
+
+function gameOver(){
+
+    alert('sry wrong answer, please try again.');
+    $('.options').hide();
+    gCurrQuest = 0;
+    renderQuestion();
+
+
+    //needs to be in delay, for User exp.
+    $('.options').show();
+
+}
+
