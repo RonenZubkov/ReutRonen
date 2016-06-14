d8c8586c7225be15eedb58d6da1a9ac7e48832da
Reut Hirshovitz <reuthi1@gmail.com>
2016-06-14 11:09:57 +0300

    changes in the chal.js

--- js/chal.js
+++ js/chal.js
@@ -1,89 +1,119 @@
 
 var gChals;
 
+
 function init(){    
 
     gChals = getChals();   
     console.log('gChals', gChals);
      
-
     renderChals(gChals, '.content');
 }
 
-function getChals() {
-    console.log('getChals');
-    
-    if (localStorage.getItem('Challenges')) gChals = JSON.parse(localStorage.getItem('Challenges'));
-    else gChals = [
-        {
-            url: 'game1.html',
-            name: 'game1',
-            isSolved: false
-        },
-        {
-            url: 'game2.html',
-            name: 'game2',
-            isSolved: false
-        },
-        {
-            url: 'game3.html',
-            name: 'game3',
-            isSolved: false
-        },
-        {
-            url: 'game4.html',
-            name: 'game4',
-            isSolved: false
-        }
-    ];
 
+function getChals() {    
+    gChals = getFromStorage('chals');
+    
+    if ( gChals === null ) {
+        gChals = [
+            {
+                id: 0,
+                url: 'game1.html',
+                name: 'game1',
+                isSolved: false
+            },
+            {
+                id: 1,                
+                url: 'game2.html',
+                name: 'game2',
+                isSolved: false
+            },
+            {
+                id: 2,                
+                url: 'game3.html',
+                name: 'game3',
+                isSolved: false
+            },
+            {
+                id: 3,                
+                url: 'game4.html',
+                name: 'game4',
+                isSolved: false
+            }
+        ];
+    }    
+    saveToStorage('chals', gChals);    
     return gChals;
 
 }
 
+
 function renderChals(chals, selector) {
   var elContainer = document.querySelector(selector);
 
-    var strHTML = '';
+    var strHTMLs = chals.map(function (chal) {
+        var strHtml = '<div onclick="return chalClicked(' + chal.id + ');" class="chal">' +
+                        '<a href="game' + (chal.id + 1) + '.html">'
+                            +'<img src="'+getImgIsSolved(chals, chal.id) + '">' +
+                            '<h2>'+ chal.name +'</h2>' + 
+                            '</a>' +
+                        '</div>';
+        
+        return strHtml;
+    })
 
-  chals.forEach(function (chal, i) {
-    strHTML += ' <div class="chals chal_' + i + '" onclick="getChalById(this, '+ i +')">' + '<h2>' + chal.name + '</h2>' 
-    + '<img src = ' + getImgIsSolved (chals, i) + '  class = img-responsive >' +'</div>'
-  });
-
-    console.log(strHTML);
-    elContainer.innerHTML = strHTML;
+    elContainer.innerHTML = strHTMLs.join('');
 }
 
-function getImgIsSolved (chals, chalsId) {
+
+function getImgIsSolved (chal, chalId) {
     var imgSrc;
-      if (chals[chalsId].isSolved === false) imgSrc = "img/img_index/lock.jpg";
+      if (chal[chalId].isSolved === false) imgSrc = "img/img_index/lock.jpg";
       else imgSrc = "img/img_index/V.png"
       return imgSrc;
   }
 
-function getChalById(chal, chalId) {
-    for(var i = 0; i < chalId; i++){
-        if(gChals[i].isSolved === false) return;
-    }
-    window.location.href = gChals[chalId].url;
+
+  function getCurrChal () {
+      var currChal;
+      for (var i = 0; i < gChals.length; i++) {
+          if (!gChals[i].isSolved) {
+              currChal = gChals[i];
+              return currChal;
+          }
+      }
+  }
+
+
+  function chalClicked (chalId) {
+        var chelClicked = getChalById(chalId)
+        var currChal = getCurrChal(chalId);              
+        if (chelClicked.id === currChal.id) console.log('chelClicked.id', chelClicked.id, 'currChal.id', currChal.id);
+        else return false;
+  }
+
+
+function getChalById(chalId) {
+    return gChals[chalId]    
 }
 
+
 function reportSolved(chalId){
-    getChals();
-    console.log(gChals);
+    gChals = getFromStorage('chals');
     
     gChals[chalId].isSolved = true;
 
-    saveToStorage('challenges',gChals);
+    saveToStorage('chals', gChals);
+    
+    //change the buttons to a href:
     window.location.href = 'index.html';
-    console.log(gChals);
 }
 
 
 function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
+
 
 function getFromStorage(key) {
     var str = localStorage.getItem(key);
