e125bdd71722cf6be2b6c78a990f664fecd10eda
RonenZubkov <ronenownz@gmail.com>
2016-06-14 13:13:39 +0300

    Game 1 - 85%

--- js/game1.js
+++ js/game1.js
@@ -9,53 +9,56 @@ var gQuestsTree = [
         id: 1,
         txt: 'Who was created by Gepetto the woodcarver??',
         opts:['a','b','c','Pinokio'],
-        correctOpt: 'Pinokio',
-        img:''
+        correctOpt: 3,
+        lifeSave: true
     },
 
     {
         id: 2,
         txt: 'Who painted the Mona Lisa?',
         opts:['a','Leonardo da Vinci','c','d'],
-        correctOpt: 'Leonardo da Vinci',
-        img:''
+        correctOpt: 2,
+        lifeSave: true
     },
 
     {
         id: 3,
         txt: 'Who is the bear in The Jungle Book??',
-        opts:['a','b','Balue','d'],
-        correctOpt: 'Balue',
-        img:''
+        opts:['a','b','Baloo','d'],
+        correctOpt: 3,
+        lifeSave: true
     }
 ];
 
 
-
-
 function init(){
-    renderQuestion();
+    renderQuest();
+    // console.log('Initied');
 }
 
 
-function renderQuestion(){
-    var elOptions = document.querySelectorAll('.options');
+function renderQuest(){
+    var elOptions = document.querySelector('.options');
+    var elImg = document.querySelector('.frame');
+    elImg.innerHTML = '<img src="img/game1Img/pic'+ gQuestsTree[gCurrQuest].id + '.jpg" class="img-rounded img-responsive" alt="">';
 
-    [].forEach.call(elOptions, function(elOption,i) {
-        elOption.innerHTML = gQuestsTree[gCurrQuest].opts[i];
+
+    var strHtmls = gQuestsTree[gCurrQuest].opts.map(function (opt,i) {
+        var strHtml = '<li class="option" onclick="checkOpt('+i+')">' + opt + '</li>';
+        return strHtml;
     });
 
+    elOptions.innerHTML = strHtmls.join('');
 }
 
 
-function checkAnswer(el){
+function checkOpt(i){
 
-    var elInpAnswer = el;
 
-    if(elInpAnswer === gQuestsTree[gCurrQuest].correctOpt){
+    if(i === gQuestsTree[gCurrQuest].correctOpt){
         alert('Correct!');
         gCurrQuest++;
-        renderQuestion();
+        renderQuest();
     }
 
     else{
@@ -70,11 +73,46 @@ function gameOver(){
     alert('sry wrong answer, please try again.');
     $('.options').hide();
     gCurrQuest = 0;
-    renderQuestion();
-
-
+    renderQuest();
     //needs to be in delay, for User exp.
-    $('.options').show();
+    // $('.options').show();
 
 }
 
+
+
+function lifeSaver(){
+    var min = 0;
+    var max = 4;
+    var currQuest = gQuestsTree[gCurrQuest];
+
+    if(gQuestsTree[gCurrQuest].lifeSave) {
+        var randNum1 = getRandomIntInclusive(min, max);
+        var randNum2 = getRandomIntInclusive(min, max);
+
+        if (randNum1 === randNum2) {
+            randNum2 = getRandomIntInclusive(0, 4);
+        }
+
+        var $optDrop1 = $('.option' + randNum1);
+        var $optDrop2 = $('.option' + randNum2);
+
+        console.log('random1: ', randNum1);
+        console.log('random2: ', randNum2);
+
+        if($optDrop1.html()!== currQuest.correctOpt && $optDrop2.html()!== currQuest.correctOpt){
+            $optDrop1.hide();
+            $optDrop2.hide();
+        }
+        else{
+            //How can i get out from this bugs :|
+        }
+        currQuest.lifeSave = false;
+    }
+
+}
+
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
