f87d32ef10fe82229f639f59648a04373388cad4
reuthi <reuthi1@gmail.com>
2016-06-16 09:38:36 -0700

    fixing game2, droppable ang dragabble, and starting game3

--- js/game2.js
+++ js/game2.js
@@ -32,14 +32,13 @@ function buildBoard (SIZE) {
 
 function renderBoard(board, selector) {
     var elContainer = document.querySelector(selector);
-
+ 
     var strHTMLs = board.map(function (chal, i) {
-        var strHtml = '<div id="droppable_'+i+'" class="droppable cell cell_' + i + '" data-type="'+i+'">' + i + '</div>'
-        // " ondrop="drop(event)" ondragover="allowDrop(event)" 
+        var strHtml = '<div id="droppable_'+i+'" class="droppable_'+i+' cell cell_' + i + '" data-type="'+i+'">' + i + '</div>'
         return strHtml;
         
     })
-
+    
     elContainer.innerHTML = strHTMLs.join('');
 }
 
@@ -58,21 +57,13 @@ function renderTheHiddens() {
     var strHtml = '';
     for (var i = 0; i < gLevels[gState.currLevel].numsToHide.length; i++) {
         console.log(gLevels[gState.currLevel].numsToHide[i])
-        strHtml += '<div id="draggable" class="draggable draggable_'+i+' drag num_' + i + '" data-type="'+i+'">' + gBoard[gLevels[gState.currLevel].numsToHide[i]] + '</div>'
-        // +'draggable="true" ondragstart="drag(event)"'        
+        strHtml += '<div class="draggable draggable_'+i+' drag num_' + i + '" data-type="'+i+'">' + gBoard[gLevels[gState.currLevel].numsToHide[i]] + '</div>'     
     }
-        console.log(strHtml)
-      var elContainer = document.querySelector('.theHiddensContainer');
-        elContainer.innerHTML = strHtml;
-        // $('.draggable').draggable({ revert:true })
-        // $('.droppable').droppable(
-        //     { accept: '.cell' }
-        // );     
-        // console.log('drop')
+    var elContainer = document.querySelector('.theHiddensContainer');
+    elContainer.innerHTML = strHtml;
     $(function() {
         $(".draggable").draggable({
-            revert: true,
-            helper: 'clone',
+            revert: "invalid",
             start: function(event, ui) {
                 $(this).fadeTo('fast', 0.5);
             },
@@ -80,72 +71,14 @@ function renderTheHiddens() {
                 $(this).fadeTo(0, 1);
             }
         });
-        
-        console.log($("#droppable_4"))
-        console.log( document.querySelector('.draggable_0'))
        
-        $("#droppable_4").droppable(
-            {
+        $(".droppable_4").droppable({
             accept: ".draggable_0",
-            // tolerance:"touch",
-            drop: drop()
         });
-        
-        function drop () {
-        console.log("hello");
-    }
-        // $("#droppable").droppable({
-        //     accept: '.cell',
-        //     hoverClass: 'active',
-        //     drop: function(event, ui) {
-        //         console.log(event)
-        //         this.value = $(ui.draggable).text();
-        //         this.css('color', 'red')
-        //     }
-        // });
     });
 }
 
-    
-    // $(".droppable_1").droppable({
-	// 	accept: ".word2"
-    // });
-    
-    // $(".droppable_1").droppable({
-	// 	accept: ".word3"
-    // });
-    
-// });
-
-
-
-
-// function allowDrop(ev) {
-//     ev.preventDefault();
-//     gState.target = ev.target.innerText
-// }
-
-// function drag(ev) {
-//     ev.dataTransfer.setData("text", ev.target.id);
-//     gState.dragged = ev.target.innerHTML;
-// }
-
-// function drop(ev) {
-//     ev.preventDefault();
-//     var data = ev.dataTransfer.getData("text");
-//     if (gState.dragged === gState.target) {
-//     console.log(gState.dragged, gState.target)
-//     ev.target.replaceChild(document.getElementById(data), document.getElementById(ev.dataTransfer.getData("text")));
-//     ev.target.style.color = 'black';
-//     var elContainer = document.querySelector('.theHiddensContainer');        
-//     var elHidden = document.getElementById(data)
-//     console.log('elHidden', elHidden)
-//     elContainer.removeChild(elHidden);
-//     checkStageComplited()
-//     }
-    
-// }
-
+ 
 
 function checkLevelComplited() {
     var elContainer = document.querySelector('.theHiddensContainer');    
