b176a841595af54bddf23458eee7ee7626f6aacd
RonenZubkov <ronenownz@gmail.com>
2016-06-17 17:11:14 +0300

    Working on responsive nav bar.

--- js/chal.js
+++ js/chal.js
@@ -7,6 +7,7 @@ function init(){
     gChals = getChals();
      
     renderChals(gChals, '.content');
+    progressBar();
 }
 
 
@@ -115,39 +116,15 @@ function getFromStorage(key) {
     return JSON.parse(str);
 }
 
+function progressBar(){
+    var elPorgress = document.querySelector('.progressBar');
+    var progress = 0;
 
-//
-// var ProgressBar = require('progressbar.js');
-// var line = new ProgressBar.Line('#container');
-//
-// var bar = new ProgressBar.Circle(container, {
-//     color: '#aaa',
-//     // This has to be the same size as the maximum width to
-//     // prevent clipping
-//     strokeWidth: 4,
-//     trailWidth: 1,
-//     easing: 'easeInOut',
-//     duration: 1400,
-//     text: {
-//         autoStyleContainer: false
-//     },
-//     from: { color: '#aaa', width: 1 },
-//     to: { color: '#333', width: 4 },
-//     // Set default step function for all animate calls
-//     step: function(state, circle) {
-//         circle.path.setAttribute('stroke', state.color);
-//         circle.path.setAttribute('stroke-width', state.width);
-//
-//         var value = Math.round(circle.value() * 100);
-//         if (value === 0) {
-//             circle.setText('');
-//         } else {
-//             circle.setText(value);
-//         }
-//
-//     }
-// });
-// bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-// bar.text.style.fontSize = '2rem';
-//
-// bar.animate(1.0);  // Number from 0.0 to 1.0
+    for(var i = 0; i < gChals.length;i++){
+        if(gChals[i].isSolved)
+        progress += (100 / gChals.length);
+        elPorgress.innerHTML = (progress + '%') ;
+        elPorgress.style.width = (progress + '%');
+    }
+}
+
